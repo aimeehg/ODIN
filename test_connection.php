4c8dd7c74b393397d1ef6b665b52b6a2d82e09e9
Aimee Hernández <zvok59@gmail.com>
2016-06-01 22:55:04 -0500

    test
    connection php ddb

---
+++ test_connection.php
@@ -0,0 +1,22 @@
+<?php
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+require("connect.php");
+$connect = mysqli_connect($servidor, $usuario, $contra,$base) or die("Unable to Connect to '$servidor'");
+mysqli_select_db($connect,$base) or die("Could not open the db '$base'");
+
+$test_query = "SHOW TABLES FROM $base";
+
+$result = mysql_query($test_query);
+
+$tblCnt = 0;
+
+while($tbl = mysql_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
+}
+if (!$tblCnt) {
+  echo "There are no tables<br />\n";
+} else {
+  echo "There are $tblCnt tables<br />\n";
+}
